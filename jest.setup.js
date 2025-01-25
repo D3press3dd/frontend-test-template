91256883a690817a249cd0ea6a3ab8a4a751320c
anthony <anthony.rosman_ext@atrapalo.com>
2025-01-24 22:43:56 -0500

    chore: update configuration files

---
+++ jest.setup.js
@@ -0,0 +1,45 @@
+import '@testing-library/jest-dom'
+
+jest.mock('next/image', () => ({
+  __esModule: true,
+  default: (props) => {
+    const { priority, loading, unoptimized, objectFit, fill, ...rest } = props
+    return <img {...rest} />
+  }
+}))
+
+jest.mock('next/link', () => ({
+  __esModule: true,
+  default: ({ children, ...props }) => {
+    return <a {...props}>{children}</a>
+  }
+}))
+
+jest.mock('next/navigation', () => ({
+  useRouter: jest.fn(() => ({
+    push: jest.fn()
+  })),
+  useSearchParams: jest.fn(() => ({
+    get: jest.fn(),
+    toString: jest.fn()
+  }))
+}))
+
+const localStorageMock = (function () {
+  let store = {}
+  return {
+    getItem: function (key) {
+      return store[key] || null
+    },
+    setItem: function (key, value) {
+      store[key] = value.toString()
+    },
+    clear: function () {
+      store = {}
+    }
+  }
+})()
+
+Object.defineProperty(window, 'localStorage', {
+  value: localStorageMock
+})
