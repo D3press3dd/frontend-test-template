5738cd38640847a474b3c450c0ff122c6713e134
anthony <anthony.rosman_ext@atrapalo.com>
2025-01-24 22:58:12 -0500

    feat: add genre context

---
+++ src/contexts/genre-context/genre-context.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { type ReactNode } from 'react'
+import type React from 'react'
+import { createContext, useContext } from 'react'
+
+interface GenreContextType {
+  genres: string[]
+}
+
+const GenreContext = createContext<GenreContextType | undefined>(undefined)
+
+export const useGenreContext = () => {
+  const context = useContext(GenreContext)
+  if (context === undefined) {
+    throw new Error('useGenreContext must be used within a GenreProvider')
+  }
+  return context
+}
+
+interface GenreProviderProps {
+  children: ReactNode
+  genres: string[]
+}
+
+export const GenreProvider: React.FC<GenreProviderProps> = ({
+  children,
+  genres
+}) => {
+  return (
+    <GenreContext.Provider value={{ genres }}>{children}</GenreContext.Provider>
+  )
+}
